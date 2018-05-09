577b362926639cd8c4743377495930e449d0ee5c
Laura Zodrow <laura.zodrow@gmail.com>
2018-05-08 20:15:39 -0600

    update dist

--- pages/Layout.js
+++ pages/Layout.js
@@ -13,8 +13,6 @@ const Layout = ({ children }) => (
       <meta name="description" content="Laura Zodrow Design and Development" />
       <meta name="viewport" content="width=device-width, initial-scale=1" />
       <link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet" />
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/muicss/0.9.35/extra/mui-noglobals.min.css" rel="stylesheet" type="text/css" media="screen" />
-      <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" />
       <meta http-equiv="Content-Language" content="en"/>
     </Head>
     <Header/>
